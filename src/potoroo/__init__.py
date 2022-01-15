fcc10ae85542c73a7247afd8a08a2a23ca76c358
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-15 11:02:46 -0500

    Bump version: 0.2.0 → 0.2.1

--- src/potoroo/__init__.py
+++ src/potoroo/__init__.py
@@ -9,6 +9,6 @@ __all__ = ["Repository"]
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
